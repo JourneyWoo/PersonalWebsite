bf3c123032324e046551e1af51e9145e1e79fae2
JourneyWoo <zw35@illinois.edu>
2019-05-04 17:48:17 -0500

    First commit

---
+++ article/views.py
@@ -0,0 +1,131 @@
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from .models import ArticlePost
+import markdown
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import ArticlePostForm
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+from comment.models import Comment
+from .models import ArticleColumn
+from comment.forms import CommentForm
+
+
+def article_list(request):
+    search = request.GET.get('search')
+    order = request.GET.get('order')
+    column = request.GET.get('column')
+    tag = request.GET.get('tag')
+
+    article_list = ArticlePost.objects.all()
+
+    if search:
+        article_list = article_list.filter(
+            Q(title__icontains=search) |
+            Q(body__icontains=search)
+        )
+    else:
+        search = ''
+
+    if column is not None and column.isdigit():
+        article_list = article_list.filter(column=column)
+
+    if tag and tag != 'None':
+        article_list = article_list.filter(tags__name__in=[tag])
+
+    if order == 'total_views':
+        article_list = article_list.order_by('-total_views')
+
+    paginator = Paginator(article_list, 10)
+    page = request.GET.get('page')
+    articles = paginator.get_page(page)
+
+    context = {
+        'articles': articles,
+        'order': order,
+        'search': search,
+        'column': column,
+        'tag': tag,
+    }
+
+    return render(request, 'article/list.html', context)
+
+
+def article_detail(request, id):
+    article = ArticlePost.objects.get(id=id)
+    comments = Comment.objects.filter(article=id)
+    comment_form = CommentForm()
+    md = markdown.Markdown(
+        extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+        ]
+    )
+    article.body = md.convert(article.body)
+
+    context = {'article': article, 'toc': md.toc, 'comments': comments, 'comment_form': comment_form,}
+    article.total_views += 1
+    article.save(update_fields=['total_views'])
+
+    return render(request, 'article/detail.html', context)
+
+
+def article_create(request):
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(request.POST, request.FILES)
+        if article_post_form.is_valid():
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=request.user.id)
+            if request.POST['column'] != 'none':
+                new_article.column = ArticleColumn.objects.get(id=request.POST['column'])
+            new_article.save()
+            article_post_form.save_m2m()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("Form ERROR, please rewrite this form.")
+    else:
+        article_post_form = ArticlePostForm()
+        columns = ArticleColumn.objects.all()
+        context = { 'article_post_form': article_post_form, 'columns': columns }
+        return render(request, 'article/create.html', context)
+
+
+@login_required(login_url='/userprofile/login/')
+def article_delete(request, id):
+    article = ArticlePost.objects.get(id=id)
+    if request.user != article.author:
+        return HttpResponse("Sorry, you don't have the permission to delete this blog.")
+    article.delete()
+    return redirect("article:article_list")
+
+
+@login_required(login_url='/userprofile/login/')
+def article_update(request, id):
+    article = ArticlePost.objects.get(id=id)
+    if request.user != article.author:
+        return HttpResponse("Sorry, you don't have the permission to edit this blog.")
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            if request.POST['column'] != 'none':
+                article.column = ArticleColumn.objects.get(id=request.POST['column'])
+            else:
+                article.column = None
+            article.save()
+            return redirect("article:article_detail", id=id)
+        else:
+            return HttpResponse("Form Wrong, please re fill")
+
+    else:
+        article_post_form = ArticlePostForm()
+        columns = ArticleColumn.objects.all()
+        context = {
+            'article': article,
+            'article_post_form': article_post_form,
+            'columns': columns,
+        }
+        return render(request, 'article/update.html', context)
